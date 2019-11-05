0f1984392a0248683499a6b5b32c7d4f8d240889
WEBmods <53878055+webmods-croatia@users.noreply.github.com>
2019-11-05 20:18:19 +0100

    More refractoring

--- common/head.php
+++ common/head.php
@@ -1,36 +1,14 @@
 <?php
-    /*
-     *      Osclass – software for creating and publishing online classified
-     *                           advertising platforms
-     *
-     *                        Copyright (C) 2014 OSCLASS
-     *
-     *       This program is free software: you can redistribute it and/or
-     *     modify it under the terms of the GNU Affero General Public License
-     *     as published by the Free Software Foundation, either version 3 of
-     *            the License, or (at your option) any later version.
-     *
-     *     This program is distributed in the hope that it will be useful, but
-     *         WITHOUT ANY WARRANTY; without even the implied warranty of
-     *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     *             GNU Affero General Public License for more details.
-     *
-     *      You should have received a copy of the GNU Affero General Public
-     * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
-     */
-?>
+$js_lang = array(
+    'delete' => __('Delete', 'matrix'),
+    'cancel' => __('Cancel', 'matrix')
+);
 
-<?php
-    $js_lang = array(
-        'delete' => __('Delete', 'matrix'),
-        'cancel' => __('Cancel', 'matrix')
-    );
-
-    osc_enqueue_script('jquery');
-    osc_enqueue_script('jquery-ui');
-    osc_register_script('global-theme-js', osc_current_web_theme_js_url('global.js'), 'jquery');
-    osc_register_script('delete-user-js', osc_current_web_theme_js_url('delete_user.js'), 'jquery-ui');
-    osc_enqueue_script('global-theme-js');
+osc_enqueue_script('jquery');
+osc_enqueue_script('jquery-ui');
+osc_register_script('global-theme-js', osc_current_web_theme_js_url('global.js'), 'jquery');
+osc_register_script('delete-user-js', osc_current_web_theme_js_url('delete_user.js'), 'jquery-ui');
+osc_enqueue_script('global-theme-js');
 ?>
 <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 
