11f21427adfd531c367deba7245ecc3b29e6aca0
WEBmods <53878055+webmods-croatia@users.noreply.github.com>
2019-12-24 19:51:45 +0100

    Head

--- head.php
+++ head.php
@@ -1,46 +1,41 @@
-<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<!DOCTYPE html>
+<html dir="<?php echo mtx_default_direction() == '0' ? 'ltr': 'rtl'; ?>" lang="<?php echo str_replace('_', '-', osc_current_user_locale()); ?>">
+    <head>
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 
-<title><?php echo meta_title() ; ?></title>
-<meta name="title" content="<?php echo osc_esc_html(meta_title()); ?>" />
-<?php if( meta_description() != '' ) { ?>
-<meta name="description" content="<?php echo osc_esc_html(meta_description()); ?>" />
-<?php } ?>
-<?php if( meta_keywords() != '' ) { ?>
-<meta name="keywords" content="<?php echo osc_esc_html(meta_keywords()); ?>" />
-<?php } ?>
-<?php if( osc_get_canonical() != '' ) { ?>
-<!-- canonical -->
-<link rel="canonical" href="<?php echo osc_get_canonical(); ?>"/>
-<!-- /canonical -->
-<?php } ?>
+        <title><?php echo meta_title(); ?></title>
+        <meta name="title" content="<?php echo osc_esc_html(meta_title()); ?>" />
+        <?php if(meta_description() != '') { ?>
+            <meta name="description" content="<?php echo osc_esc_html(meta_description()); ?>" />
+        <?php } ?>
+        <?php if(osc_get_canonical() != '') { ?>
+            <link rel="canonical" href="<?php echo osc_get_canonical(); ?>" />
+        <?php } ?>
 
-<meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
+        <meta name="viewport" content="initial-scale = 1.0, maximum-scale = 1.0" />
 
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
 
-<!-- favicon -->
-<link rel="shortcut icon" href="<?php echo osc_current_web_theme_url('favicon/favicon-48.png'); ?>">
-<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo osc_current_web_theme_url('favicon/favicon-144.png'); ?>">
-<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo osc_current_web_theme_url('favicon/favicon-114.png'); ?>">
-<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo osc_current_web_theme_url('favicon/favicon-72.png'); ?>">
-<link rel="apple-touch-icon-precomposed" href="<?php echo osc_current_web_theme_url('favicon/favicon-57.png'); ?>">
-<!-- /favicon -->
+        <link rel="shortcut icon" href="<?php echo osc_current_web_theme_url('favicon/favicon-48.png'); ?>">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo osc_current_web_theme_url('favicon/favicon-144.png'); ?>">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo osc_current_web_theme_url('favicon/favicon-114.png'); ?>">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo osc_current_web_theme_url('favicon/favicon-72.png'); ?>">
+        <link rel="apple-touch-icon-precomposed" href="<?php echo osc_current_web_theme_url('favicon/favicon-57.png'); ?>">
 
-<link href="<?php echo osc_current_web_theme_url('js/jquery-ui/jquery-ui-1.10.2.custom.min.css') ; ?>" rel="stylesheet" type="text/css" />
+        <script type="text/javascript">
+            var matrix = window.matrix || {};
+            matrix.base_url = '<?php echo osc_base_url(1); ?>';
+            matrix.langs = <?php echo json_encode(array('delete' => __('Delete', 'matrix'), 'cancel' => __('Cancel', 'matrix'))); ?>;
+            matrix.fancybox_prev = '<?php echo osc_esc_js(__('Previous image', 'matrix')); ?>';
+            matrix.fancybox_next = '<?php echo osc_esc_js(__('Next image', 'matrix')); ?>';
+            matrix.fancybox_closeBtn = '<?php echo osc_esc_js(__('Close', 'matrix')); ?>';
+            matrix.repeat_password = '<?php echo osc_esc_js(__('Passwords aren\'t matching.', 'matrix')); ?>';
+            matrix.confirm = '<?php echo osc_esc_js(__('This action can\'t be undone. Are you sure you want to continue?', 'matrix')); ?>';
+        </script>
 
-<script type="text/javascript">
-    var matrix = window.matrix || {};
-    matrix.base_url = '<?php echo osc_base_url(1); ?>';
-    matrix.langs = <?php echo json_encode(array('delete' => __('Delete', 'matrix'), 'cancel' => __('Cancel', 'matrix'))); ?>;
-    matrix.fancybox_prev = '<?php echo osc_esc_js(__('Previous image', 'matrix')); ?>';
-    matrix.fancybox_next = '<?php echo osc_esc_js(__('Next image', 'matrix')); ?>';
-    matrix.fancybox_closeBtn = '<?php echo osc_esc_js(__('Close', 'matrix')); ?>';
-    matrix.repeat_password = '<?php echo osc_esc_js(__('Passwords aren\'t matching.', 'matrix')); ?>';
-    matrix.confirm = '<?php echo osc_esc_js(__('This action can\'t be undone. Are you sure you want to continue?', 'matrix')); ?>';
-</script>
+        <?php osc_run_hook('header'); ?>
 
-<?php osc_run_hook('header') ; ?>
-
-<!-- temporary -->
-<link href="https://fonts.googleapis.com/css?family=Open+Sans|Proza+Libre|Righteous&display=swap" rel="stylesheet">
+        <!-- temporary -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans|Proza+Libre|Righteous&display=swap" rel="stylesheet">
+    </head>
--- header.php
+++ header.php
@@ -1,105 +1,101 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" dir="<?php echo mtx_default_direction()=='0' ? 'ltr': 'rtl'; ?>" lang="<?php echo str_replace('_', '-', osc_current_user_locale()); ?>">
-    <head>
-        <?php osc_current_web_theme_path('head.php') ; ?>
-    </head>
-    <body <?php mtx_body_class(); ?>>
-        <header class="header">
-            <nav class="navbar navbar-expand-lg navbar-dark bg-accent">
-                <div class="container">
-                    <a class="navbar-brand" href="<?php echo osc_base_url(); ?>"><?php echo mtx_logo('header'); ?></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNav" aria-controls="mainNav" aria-expanded="false" aria-label="<?php _e('Toggle navigation', 'matrix'); ?>">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+<?php osc_current_web_theme_path('head.php'); ?>
+<body <?php mtx_body_class(); ?>>
+    <header class="header">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-accent">
+            <div class="container">
+                <a class="navbar-brand" href="<?php echo osc_base_url(); ?>"><?php echo mtx_logo('header'); ?></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNav" aria-controls="mainNav" aria-expanded="false" aria-label="<?php _e('Toggle navigation', 'matrix'); ?>">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-                    <div class="collapse navbar-collapse" id="mainNav">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="<?php echo osc_base_url(); ?>"><?php _e('Home', 'matrix'); ?> <span class="sr-only">(<?php _e('current', 'matrix'); ?>)</span></a>
+                <div class="collapse navbar-collapse" id="mainNav">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="<?php echo osc_base_url(); ?>"><?php _e('Home', 'matrix'); ?> <span class="sr-only">(<?php _e('current', 'matrix'); ?>)</span></a>
+                        </li>
+
+                        <li class="nav-item nav-contact">
+                            <a class="nav-link" href="<?php echo osc_contact_url(); ?>"><?php _e('Contact', 'matrix'); ?></a>
+                        </li>
+
+                        <?php if(osc_users_enabled() || (!osc_users_enabled() && !osc_reg_user_post())) { ?>
+                            <li class="nav-item nav-publish">
+                                <a class="nav-link" href="<?php echo osc_item_post_url_in_category(); ?>"><?php _e('Post an ad', 'matrix'); ?></a>
                             </li>
-
-                            <li class="nav-item nav-contact">
-                                <a class="nav-link" href="<?php echo osc_contact_url(); ?>"><?php _e('Contact', 'matrix'); ?></a>
-                            </li>
-
-                            <?php if(osc_users_enabled() || (!osc_users_enabled() && !osc_reg_user_post())) { ?>
-                                <li class="nav-item nav-publish">
-                                    <a class="nav-link" href="<?php echo osc_item_post_url_in_category(); ?>"><?php _e('Post an ad', 'matrix'); ?></a>
+                        <?php } ?>
+                    </ul>
+                    <ul class="navbar-nav my-2 my-lg-0">
+                        <?php if(osc_users_enabled()) { ?>
+                            <?php if(osc_is_web_user_logged_in()) { ?>
+                                <li class="nav-item nav-welcome">
+                                    <?php echo sprintf(__('Hi %s', 'matrix'), osc_logged_user_name() . '!'); ?>
                                 </li>
-                            <?php } ?>
-                        </ul>
-                        <ul class="navbar-nav my-2 my-lg-0">
-                            <?php if(osc_users_enabled()) { ?>
-                                <?php if(osc_is_web_user_logged_in()) { ?>
-                                    <li class="nav-item nav-welcome">
-                                        <?php echo sprintf(__('Hi %s', 'matrix'), osc_logged_user_name() . '!'); ?>
+                                <li class="nav-item nav-account">
+                                    <a href="<?php echo osc_user_dashboard_url(); ?>" class="nav-link"><?php _e('My account', 'matrix'); ?></a>
+                                </li>
+                                <li class="nav-item nav-logout">
+                                    <a href="<?php echo osc_user_logout_url(); ?>" class="nav-link"><?php _e('Logout', 'matrix'); ?></a>
+                                </li>
+                            <?php } else { ?>
+                                <li class="nav-item nav-login">
+                                    <a href="<?php echo osc_user_login_url(); ?>" class="nav-link text-white"><?php _e('Login', 'matrix'); ?></a>
+                                </li>
+                                <?php if(osc_user_registration_enabled()) { ?>
+                                    <li class="nav-item nav-register">
+                                        <a href="<?php echo osc_register_account_url(); ?>" class="nav-link text-white"><?php _e('Register', 'matrix'); ?></a>
                                     </li>
-                                    <li class="nav-item nav-account">
-                                        <a href="<?php echo osc_user_dashboard_url(); ?>" class="nav-link"><?php _e('My account', 'matrix'); ?></a>
-                                    </li>
-                                    <li class="nav-item nav-logout">
-                                        <a href="<?php echo osc_user_logout_url(); ?>" class="nav-link"><?php _e('Logout', 'matrix'); ?></a>
-                                    </li>
-                                <?php } else { ?>
-                                    <li class="nav-item nav-login">
-                                        <a href="<?php echo osc_user_login_url(); ?>" class="nav-link text-white"><?php _e('Login', 'matrix'); ?></a>
-                                    </li>
-                                    <?php if(osc_user_registration_enabled()) { ?>
-                                        <li class="nav-item nav-register">
-                                            <a href="<?php echo osc_register_account_url(); ?>" class="nav-link text-white"><?php _e('Register', 'matrix'); ?></a>
-                                        </li>
-                                    <?php } ?>
                                 <?php } ?>
                             <?php } ?>
-                        </ul>
-                    </div>
+                        <?php } ?>
+                    </ul>
                 </div>
-            </nav>
+            </div>
+        </nav>
 
-            <?php mtx_flash(); ?>
+        <?php mtx_flash(); ?>
 
-            <?php if(osc_is_home_page()) { ?>
-                <section class="jumbo">
-                    <div class="jumbotron bg-darker text-white mb-0">
-                        <div class="container">
-                            <div class="row">
-                                <div class="jumbo-text jumbo-margin col-md-8">
-                                    <h1 class="display-4"><?php _e('Modern & open-source', 'matrix'); ?></h1>
-                                    <p class="lead"><?php _e('Osclass theme', 'matrix'); ?></p>
-                                    <p class="mt-5"><?php _e('What are you waiting for?', 'matrix'); ?></p>
-                                    <p class="lead">
-                                        <a class="btn btn-lg btn-mtx bg-accent" href="<?php echo osc_item_post_url_in_category(); ?>"><?php _e('Post an ad', 'matrix'); ?></a>
-                                    </p>
-                                </div>
-                                <div class="jumbo-search col-md-4">
-                                    <form action="<?php echo osc_base_url(1); ?>" method="GET" class="nocsrf text-white bg-accent">
-                                        <input type="hidden" name="page" value="search"/>
-                                        <div class="p-4">
-                                            <div class="form-group">
-                                                <label for="sPattern"><?php _e('Query', 'matrix'); ?></label>
-                                                <input type="text" class="form-control" id="sPattern" name="sPattern" placeholder="<?php echo osc_esc_html(osc_get_preference('keyword_placeholder', 'matrix')); ?>">
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="sCategory"><?php _e('Category', 'matrix'); ?></label>
-                                                <?php mtx_search_category_select(); ?>
-                                            </div>
+        <?php if(osc_is_home_page()) { ?>
+            <section class="jumbo">
+                <div class="jumbotron bg-darker text-white mb-0">
+                    <div class="container">
+                        <div class="row">
+                            <div class="jumbo-text jumbo-margin col-md-8">
+                                <h1 class="display-4"><?php _e('Modern & open-source', 'matrix'); ?></h1>
+                                <p class="lead"><?php _e('Osclass theme', 'matrix'); ?></p>
+                                <p class="mt-5"><?php _e('What are you waiting for?', 'matrix'); ?></p>
+                                <p class="lead">
+                                    <a class="btn btn-lg btn-mtx bg-accent" href="<?php echo osc_item_post_url_in_category(); ?>"><?php _e('Post an ad', 'matrix'); ?></a>
+                                </p>
+                            </div>
+                            <div class="jumbo-search col-md-4">
+                                <form action="<?php echo osc_base_url(1); ?>" method="GET" class="nocsrf text-white bg-accent">
+                                    <input type="hidden" name="page" value="search"/>
+                                    <div class="p-4">
+                                        <div class="form-group">
+                                            <label for="sPattern"><?php _e('Query', 'matrix'); ?></label>
+                                            <input type="text" class="form-control" id="sPattern" name="sPattern" placeholder="<?php echo osc_esc_html(osc_get_preference('keyword_placeholder', 'matrix')); ?>">
                                         </div>
                                         <div class="form-group">
-                                            <button type="submit" class="btn btn-mtx bg-accent-dark w-100"><?php _e('Search', 'matrix'); ?></button>
+                                            <label for="sCategory"><?php _e('Category', 'matrix'); ?></label>
+                                            <?php mtx_search_category_select(); ?>
                                         </div>
-                                    </form>
-                                </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-mtx bg-accent-dark w-100"><?php _e('Search', 'matrix'); ?></button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
-                </section>
-            <?php } else { ?>
-                <?php osc_run_hook('mtx_jumbo'); ?>
-            <?php } ?>
-        </header>
+                </div>
+            </section>
+        <?php } else { ?>
+            <?php osc_run_hook('mtx_jumbo'); ?>
+        <?php } ?>
+    </header>
 
-        <?php osc_show_widgets('header'); ?>
+    <?php osc_show_widgets('header'); ?>
 
-        <?php osc_run_hook('before-main'); ?>
-        <main class="main">
-            <?php osc_run_hook('inside-main'); ?>
+    <?php osc_run_hook('before-main'); ?>
+    <main class="main">
+        <?php osc_run_hook('inside-main'); ?>
