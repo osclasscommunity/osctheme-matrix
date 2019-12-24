3acb2f842afe74eafc1e2f26c218fb296bb792da
WEBmods <53878055+webmods-croatia@users.noreply.github.com>
2019-12-24 18:24:38 +0100

    Image hover

--- loop-single-premium.php
+++ loop-single-premium.php
@@ -1,13 +1,17 @@
 <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6 mb-3">
     <div class="card ad premium <?php osc_run_hook('highlight_class'); ?> mb-3 h-100">
-        <span class="ad-premium badge bg-accent text-white p-2"><?php _e('Premium', 'matrix'); ?></span>
-        <?php if(osc_images_enabled_at_items()) { ?>
-            <?php if(osc_count_premium_resources()) { ?>
-                <img src="<?php echo osc_resource_thumbnail_url(); ?>" class="card-img-top" alt="<?php echo osc_esc_html(osc_premium_title()) ; ?>">
-            <?php } else { ?>
-                <img src="<?php echo osc_current_web_theme_url('images/nophoto.svg'); ?>" class="card-img-top" alt="<?php echo osc_esc_html(osc_premium_title()) ; ?>">
+        <div class="card-img-wrap">
+            <span class="ad-premium badge bg-accent text-white p-2"><?php _e('Premium', 'matrix'); ?></span>
+            <?php if(osc_images_enabled_at_items()) { ?>
+                <a href="<?php echo osc_item_url(); ?>">
+                    <?php if(osc_count_premium_resources()) { ?>
+                        <img src="<?php echo osc_resource_thumbnail_url(); ?>" class="card-img-top" alt="<?php echo osc_esc_html(osc_premium_title()); ?>">
+                    <?php } else { ?>
+                        <img src="<?php echo osc_current_web_theme_url('images/nophoto.svg'); ?>" class="card-img-top" alt="<?php echo osc_esc_html(osc_premium_title()); ?>">
+                    <?php } ?>
+                </a>
             <?php } ?>
-        <?php } ?>
+        </div>
         <div class="card-body text-center">
             <h5 class="card-title"><a href="<?php echo osc_premium_url(); ?>" class="cl-darker"><?php echo osc_highlight(osc_premium_title(), 100); ?></a></h5>
             <div class="card-text">
--- loop-single.php
+++ loop-single.php
@@ -1,15 +1,21 @@
 <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6 mb-3">
     <div class="card ad <?php osc_run_hook('highlight_class'); ?> mb-3 h-100">
-        <?php if(osc_item_is_premium()) { ?>
-            <span class="ad-premium badge bg-accent text-white p-2"><?php _e('Premium', 'matrix'); ?></span>
-        <?php } ?>
-        <?php if(osc_images_enabled_at_items()) { ?>
-            <?php if(osc_count_item_resources()) { ?>
-                <img src="<?php echo osc_resource_thumbnail_url(); ?>" class="card-img-top" alt="<?php echo osc_esc_html(osc_item_title()) ; ?>">
-            <?php } else { ?>
-                <img src="<?php echo osc_current_web_theme_url('images/nophoto.svg'); ?>" class="card-img-top" alt="<?php echo osc_esc_html(osc_item_title()) ; ?>">
+        <div class="card-img-wrap">
+            <?php if(osc_item_is_premium()) { ?>
+                <span class="ad-premium badge bg-accent text-white p-2"><?php _e('Premium', 'matrix'); ?></span>
             <?php } ?>
-        <?php } ?>
+            <?php if(osc_images_enabled_at_items()) { ?>
+                <div class="card-img-wrap">
+                    <a href="<?php echo osc_item_url(); ?>">
+                        <?php if(osc_count_item_resources()) { ?>
+                            <img src="<?php echo osc_resource_thumbnail_url(); ?>" class="card-img-top" alt="<?php echo osc_esc_html(osc_item_title()); ?>">
+                        <?php } else { ?>
+                            <img src="<?php echo osc_current_web_theme_url('images/nophoto.svg'); ?>" class="card-img-top" alt="<?php echo osc_esc_html(osc_item_title()); ?>">
+                        <?php } ?>
+                    </a>
+                </div>
+            <?php } ?>
+        </div>
         <div class="card-body text-center">
             <h5 class="card-title"><a href="<?php echo osc_item_url(); ?>" class="cl-darker"><?php echo osc_highlight(osc_item_title(), 100); ?></a></h5>
             <div class="card-text">
