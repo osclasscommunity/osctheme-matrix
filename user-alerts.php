ca41c3c5f0ad6fa75c1baaf4e97e7715b1863753
WEBmods <53878055+webmods-croatia@users.noreply.github.com>
2020-01-03 22:51:28 +0100

    Optimized classes on alerts page
    Still needs work...

--- user-alerts.php
+++ user-alerts.php
@@ -13,29 +13,33 @@ $alerts = View::newInstance()->_get('alerts');
             <section class="user-alerts">
                 <h1 class="cl-accent-dark"><?php _e('My alerts', 'matrix'); ?></h1>
                 <p class="cl-darker"><?php _e('Manage searches you subscribed to.', 'matrix'); ?></p>
-                <?php if(osc_count_alerts() == 0) { ?>
-                    <p class="text-center cl-darker"><?php _e('No alerts, yet.', 'matrix'); ?></p>
-                <?php } else { ?>
-                    <?php $i = 0; ?>
-                    <?php foreach($alerts as $alert) { ?>
-                        <?php $i++; $details = mtx_user_alert_parse_details($alert); ?>
-                        <div class="container bg-light">
-                            <div class="col-12 p-3">
-                                <div class="col-12">
+                <div class="container">
+                    <?php if(osc_count_alerts() == 0) { ?>
+                        <p class="text-center cl-darker"><?php _e('No alerts, yet.', 'matrix'); ?></p>
+                    <?php } else { ?>
+                        <?php $i = 0; ?>
+                        <?php foreach($alerts as $alert) { ?>
+                            <?php $i++; $details = mtx_user_alert_parse_details($alert); ?>
+                            <div class="bg-light col-12 p-3">
+                                <div>
                                     <h4><?php printf(__('Alert no. %s', 'matrix'), $i); ?></h4>
                                     <?php foreach($details as $detail) { ?>
                                         <p class="mb-0"><strong><?php echo $detail['name']; ?>:</strong> <?php echo $detail['value']; ?></p>
                                     <?php } ?>
-
                                     <a href="<?php echo osc_user_unsubscribe_alert_url($alert['pk_i_id'], $alert['s_email'], $alert['s_secret']); ?>" class="btn btn-mtx bg-accent text-white mt-3 mb-3" onclick="javascript: return confirm(matrix.confirm);"><?php _e('Unsubscribe', 'matrix'); ?></a>
                                 </div>
-                                <?php View::newInstance()->_exportVariableToView('items', $alert['items']); ?>
-                                <?php osc_current_web_theme_path('loop.php'); ?>
+                                <div class="row">
+                                    <?php View::newInstance()->_exportVariableToView('items', $alert['items']); ?>
+                                    <?php while(osc_has_items()) { ?>
+                                        <?php mtx_loop_item(false); ?>
+                                        <?php mtx_loop_item(false); ?>
+                                    <?php } ?>
+                                </div>
                             </div>
-                        </div>
-                        <div class="container"><hr></div>
+                            <hr>
+                        <?php } ?>
                     <?php } ?>
-                <?php } ?>
+                </div>
             </section>
         </div>
     </div>
